aacba30de38c6d25c5e347155d3be93c0ebc6b59
AsukaDev <80499206+AsukaDev-Official@users.noreply.github.com>
2021-04-18 22:53:50 +0700

    Add files via upload

---
+++ contacts.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""This module provides Contacts entry point script."""
+
+from contacts.main import main
+
+if __name__ == "__main__":
+    main()
